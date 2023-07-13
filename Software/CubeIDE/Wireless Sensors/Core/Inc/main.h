07f69af66f589334d879067489fa8a08cb8f1dc1
kgtissot <kevin@nselabs.com>
2023-07-13 13:45:05 -0500

    Added GPIO for one wire interface

--- Software/CubeIDE/Wireless Sensors/Core/Inc/main.h
+++ Software/CubeIDE/Wireless Sensors/Core/Inc/main.h
@@ -69,6 +69,8 @@ void Error_Handler(void);
 #define LoRa_DIO1_GPIO_Port GPIOA
 #define LoRa_NSS_Pin GPIO_PIN_4
 #define LoRa_NSS_GPIO_Port GPIOA
+#define OneWire_Pin GPIO_PIN_10
+#define OneWire_GPIO_Port GPIOB
 #define LoRa_BUSY_Pin GPIO_PIN_8
 #define LoRa_BUSY_GPIO_Port GPIOA
 
