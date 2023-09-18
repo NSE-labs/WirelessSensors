261f82a40c15d3de93e8bd64070f639ca5ceb3ed
kgtissot <kevin@nselabs.com>
2023-09-18 15:47:35 -0500

    Add deep sleep

--- Software/STM32 Arduino/Voltage_and_current_meter/Voltage_and_current_meter.ino
+++ Software/STM32 Arduino/Voltage_and_current_meter/Voltage_and_current_meter.ino
@@ -1,16 +1,23 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include "stm32yyxx_ll_adc.h"
+#include "STM32LowPower.h"
+
+
+// define this if you want the OLED display enabled
+#define DISPLAY_ENABLED 
+
+// define this if you want the SD card enabled
+// #define SD_ENABLED
+
+#ifdef SD_ENABLED
 #include <SPI.h>
 #include <SD.h>
+#endif
 
-const int chipSelect = A4;
-
-#define LL_ADC_RESOLUTION LL_ADC_RESOLUTION_12B
-#define ADC_RANGE 4096
-
+#ifdef DISPLAY_ENABLED
+#include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
@@ -18,45 +25,43 @@ const int chipSelect = A4;
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#endif
+
+#ifdef SD_ENABLED
+const int chipSelect = PA4; // pin used for SD card chip select
+#endif
+
+const int ledPin = PC13;    // on-board LED pin
+
+#define LL_ADC_RESOLUTION LL_ADC_RESOLUTION_12B
+#define ADC_RANGE 4096
 
 // Declaration for the INA219 voltage and current sensor
 Adafruit_INA219 batteryMonitor(0x40);
 Adafruit_INA219 panelMonitor(0x41);
 
+int sampleCount = 0;
+
 
 void setup(void) 
 {
-  Serial.begin(115200);
+  // Configure low power
+  LowPower.begin();
 
+#ifdef DISPLAY_ENABLED
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    while(1)
-    {
-      Serial.println("Screen allocation failed");
-      delay(1000);
-    }
-  }
-
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
   display.display();
+#endif
   
   // Initialize the INA219s.
   // By default the initialization will use the largest range (32V, 2A).  However
   // you can call a setCalibration function to change this range (see comments).
-  if (! batteryMonitor.begin()) {
-    while(1)
-    {
-      Serial.println("Failed to find battery INA219 chip");
-      delay(1000);
-    }
-  }
-  if (! panelMonitor.begin()) {
-    while(1)
-    {
-      Serial.println("Failed to find panel INA219 chip");
-      delay(1000);
-    }
-  }
+  batteryMonitor.begin();
+
+  panelMonitor.begin(); 
+  
   // To use a slightly lower 32V, 1A range (higher precision on amps):
   //ina219.setCalibration_32V_1A();
   // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
@@ -65,19 +70,16 @@ void setup(void)
 
   analogReadResolution(12);
 
+  #ifdef SD_ENABLED
   // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) 
-  {
-    Serial.println("SD card failed, or not present");
-  }
-  else
-  {
-    Serial.println("SD card initialized.");
-  }
+  while(!SD.begin(chipSelect));
+  #endif
+
   pinMode(PA0, OUTPUT); /* Charging LED */
   pinMode(PA1, OUTPUT); /* Done LED */
   pinMode(PA10, INPUT_PULLUP); /* Charging input */
   pinMode(PA15, INPUT_PULLUP); /* Done input */
+  pinMode(ledPin, OUTPUT); /* On-board LED */
 }
 
 void loop(void) 
@@ -94,6 +96,11 @@ void loop(void)
   String dataString = "";
   int chargeStatus = LOW;
   int doneStatus = LOW;
+#ifdef SD_ENABLED
+  File dataFile;
+#endif
+
+  digitalWrite(ledPin, HIGH); // Turn on-board LED on
 
   Vcc = ((float)__LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION))/1000;
   
@@ -104,7 +111,8 @@ void loop(void)
   panelVoltage = panelMonitor.getBusVoltage_V();
   panelCurrent = panelMonitor.getCurrent_mA();
   panelPower = panelMonitor.getPower_mW();
-  
+
+#ifdef DISPLAY_ENABLED
   display.clearDisplay();
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
@@ -122,25 +130,7 @@ void loop(void)
   display.setCursor(96, 8);
   display.print(Vcc); display.println(F("V"));
   display.display();
-  
-  Serial.print("Bus Voltage:    "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage:  "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Battery Voltage:"); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Battery Current:"); Serial.print(current_mA); Serial.println(" mA");
-  Serial.print("Battery Power:  "); Serial.print(power_mW); Serial.println(" mW");
-  Serial.println("");
-
-  dataString += String(busvoltage) + "," + String(current_mA) + "," + String(power_mW) + "," + String(panelVoltage) + "," + String(panelCurrent) + "," + String(panelPower) + "," + String(Vcc);
-  
-  
-  // open the log file
-  File dataFile = SD.open("log.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-  }
+#endif
 
   // Light up LEDs to reflect charging status */
   if (digitalRead(PA10) == HIGH) /* Charging status */
@@ -163,6 +153,24 @@ void loop(void)
 
   digitalWrite(PA0, chargeStatus);
   digitalWrite(PA1, doneStatus);
+
+  sampleCount++;
+
+#ifdef SD_ENABLED
+  // create string to write to log file
+  dataString += String(sampleCount) + "," + String(busvoltage) + "," + String(current_mA) + "," + String(power_mW) + "," + String(panelVoltage) + "," + String(panelCurrent) + "," + String(panelPower) + "," + String(Vcc) +"," + String(chargeStatus) + "," + String(doneStatus);
   
-  delay(2000);
+  // open the log file
+  dataFile = SD.open("log.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }
+ #endif
+
+  digitalWrite(ledPin, LOW); // Turn on-board LED off
+
+  LowPower.deepSleep(2000);
 }
